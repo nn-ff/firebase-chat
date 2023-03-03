c922792542667204eb20c7c20cbcd03dddc8d9f7
nn-ff <ilyakom3@gmail.com>
2023-03-04 01:00:00 +0500

    enter bug

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,13 +1,13 @@
 import { Button, Input } from 'antd'
 import { getAuth, signOut } from 'firebase/auth'
-import React, { FC, useState } from 'react'
+import React, { createRef, FC, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAppDispatch } from '../../hooks/useAppDispatch'
 import { useAppSelector } from '../../hooks/useAppSelector'
 import { removeUser } from '../../store/slices/userSlice'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
-import { addDoc, collection, query, serverTimestamp } from 'firebase/firestore'
+import { addDoc, collection, orderBy, query, serverTimestamp } from 'firebase/firestore'
 import { db } from '../../firebase'
 
 interface Imsg {
@@ -19,16 +19,20 @@ export const Chat: FC = () => {
   const [value, setValue] = useState<string>('')
   const [msg, setMsg] = useState<Imsg[]>([])
   const auth = getAuth()
-  const [messages, loading, error] = useCollectionData(query(collection(db, 'messages')))
+  const [messages, loading, error] = useCollectionData(
+    query(collection(db, 'messages'), orderBy('timestamp', 'asc')),
+  )
   const [user] = useAuthState(auth)
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
+  const chatRef = createRef<HTMLDivElement>()
   const { photoURL, displayName } = useAppSelector((state) => state.userSlice.user)
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value)
   }
 
-  const onClickButton = () => {
+  const onClickButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
     setMsg([...msg, { value: value, id: Date.now() }])
     setValue('')
     console.log(user)
@@ -48,7 +52,24 @@ export const Chat: FC = () => {
         displayName: user?.displayName,
         timestamp: serverTimestamp(),
       })
-      console.log('Document written with ID: ', docRef.id)
+      setValue('')
+      chatRef?.current?.scrollIntoView({ behavior: 'smooth' })
+    } catch (e) {
+      console.error('Error adding document: ', e)
+    }
+  }
+  const onSubmitHandlerMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      const docRef = await addDoc(collection(db, 'messages'), {
+        text: value,
+        uid: user?.uid,
+        photoURL: user?.photoURL,
+        displayName: user?.displayName,
+        timestamp: serverTimestamp(),
+      })
+      setValue('')
+      chatRef?.current?.scrollIntoView({ behavior: 'smooth' })
     } catch (e) {
       console.error('Error adding document: ', e)
     }
@@ -59,7 +80,7 @@ export const Chat: FC = () => {
       <Button onClick={onClickLogout} style={{ position: 'absolute', right: 0 }}>
         выйти
       </Button>
-      <div className='chat-window__wrapper'>
+      <div ref={chatRef} className='chat-window__wrapper'>
         <div style={{ marginTop: 'auto' }}></div>
         {messages?.map((obj) => {
           return (
@@ -81,8 +102,10 @@ export const Chat: FC = () => {
         })}
       </div>
       <div className='chat-window__submit'>
-        <Input onChange={onChangeHandler} type='text' value={value} />
-        <Button onClick={sendMessage}>send</Button>
+        <form onSubmit={onSubmitHandlerMessage}>
+          <Input onChange={onChangeHandler} type='text' value={value} />
+          <Button onClick={sendMessage}>send</Button>
+        </form>
       </div>
     </div>
   )
