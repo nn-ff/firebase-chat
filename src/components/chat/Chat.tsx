5362ecc4031ffdd9566aa042f5ab8797c2942150
nn-ff <ilyakom3@gmail.com>
2023-03-05 19:05:06 +0500

    ButtonLoad

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -10,12 +10,15 @@ import {
   addDoc,
   collection,
   DocumentData,
+  getDocs,
   limit,
   limitToLast,
   onSnapshot,
   orderBy,
   query,
+  QueryDocumentSnapshot,
   serverTimestamp,
+  startAfter,
 } from 'firebase/firestore'
 import { db } from '../../firebase'
 import dayjs from 'dayjs'
@@ -30,7 +33,6 @@ export const Chat: FC = () => {
   const dispatch = useAppDispatch()
   const chatRef = useRef<HTMLDivElement>(null)
   const formRef = useRef<HTMLFormElement>(null)
-
   const onChangeHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(e.target.value)
   }
@@ -67,22 +69,47 @@ export const Chat: FC = () => {
     } catch (e) {
       console.error('Error adding document: ', e)
     }
+    chatRef?.current?.scrollIntoView()
+  }
+  const [latest, setLatest] = useState<QueryDocumentSnapshot<DocumentData>>()
+
+  const onClickLoadMore = async () => {
+    const q = query(
+      collection(db, 'messages'),
+      orderBy('timestamp', 'desc'),
+      startAfter(latest),
+      limit(25),
+    )
+    const NewItems = await getDocs(q)
+    const dataitem: DocumentData[] = []
+    if (!NewItems.empty) {
+      NewItems.forEach((doc) => {
+        dataitem.push(doc.data())
+      })
+      setItems([...dataitem.reverse(), ...items])
+      setLatest(NewItems.docs[NewItems.docs.length - 1])
+      console.log(NewItems.docs)
+    } else {
+      alert('нет данных')
+    }
   }
 
   useEffect(() => {
-    const q = query(collection(db, 'messages'), orderBy('timestamp', 'asc'), limitToLast(25))
+    const q = query(collection(db, 'messages'), orderBy('timestamp', 'desc'), limit(25))
     const unsub = onSnapshot(q, (querySnapshot) => {
+      setLatest(querySnapshot.docs[querySnapshot.docs.length - 1])
       const dataitem: DocumentData[] = []
       querySnapshot.forEach((doc) => {
         dataitem.push(doc.data())
       })
-      setItems(dataitem)
+      setItems(dataitem.reverse())
     })
     return () => unsub()
   }, [])
+
   useEffect(() => {
     chatRef?.current?.scrollIntoView()
-  }, [items])
+  }, [])
   return (
     <div className='chat-window'>
       <Button onClick={onClickLogout} style={{ position: 'absolute', right: 0 }}>
@@ -90,8 +117,9 @@ export const Chat: FC = () => {
       </Button>
       <div className='chat-window__wrapper'>
         <div style={{ marginTop: 'auto' }}></div>
+        <Button onClick={onClickLoadMore}>Load more</Button>
         {items?.map((obj, index) => {
-          const messageTime = dayjs(obj?.timestamp?.toDate() || new Date()).format('hh:mm')
+          const messageTime = dayjs(obj?.timestamp?.toDate() || new Date()).format('HH:mm')
           const indexconverter = items[index - 1]?.uid ? items[index - 1].uid : 'false'
           return (
             <div key={obj.timestamp} style={{ display: 'flex' }} className='chat-window__msg'>
